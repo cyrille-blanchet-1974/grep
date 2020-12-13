8de797255a309bdc8c361c3a689191be54e6385d
Cyrille Blanchet <cyrille.blanchet@gmail.com>
2020-12-13 19:43:34 +0100

    adding test scripts

---
+++ test.cmd
@@ -0,0 +1,20 @@
+@echo off
+rem 1
+@cargo build 
+rem 3
+rem 4
+rem 5
+rem 6
+rem 7
+rem 8
+rem 9
+.\target\debug\grep.exe rem test.cmd
+rem A
+rem B
+rem C
+rem D
+rem E
+rem F
+rem G
+pause
+.\target\debug\grep.exe rem test.cmd -after3 -before3
---
+++ test.sh
@@ -0,0 +1,20 @@
+#/bin/sh*
+# 1
+cargo build 
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+./target/debug/grep # test.sh
+# A
+# B
+# C
+# D
+# E
+# F
+# G
+read a
+./target/debug/grep.exe # test.sh -after3 -before3
