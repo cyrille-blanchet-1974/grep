00046502431a982eacb687b768c05ac56f241a4c
Cyrille Blanchet <cyrille.blanchet@gmail.com>
2020-12-19 19:19:26 +0100

    recursion in file to search via glob crate

--- src/grep.rs
+++ src/grep.rs
@@ -11,9 +11,19 @@ pub fn start_thread_grep(from_aggregate: Receiver<Simplelineaggregate>, data: &P
     spawn(move || {
         let mut stdout = StandardStream::stdout(ColorChoice::Always);
         let mut founds=0;
+        let mut prec_file="".to_string();
         for l in from_aggregate {
             if l.where_to_search.contains(&to_search){
                 founds+=1;
+                let mut separate = l.to_display.len() > 1;//if more than one ligne to display
+                if prec_file != "" && prec_file!=l.file{ //or if we change of file
+                    separate = true;
+                    prec_file.clear();
+                    prec_file.push_str(&l.file);
+                }
+                if separate { //then we display an empty line
+                    println!();
+                }
                 for td in l.to_display{
                     write!(&mut stdout, "N°{} File:{}(line:{})==>", founds,&l.file,l.position).unwrap();               
                     if td == l.where_to_search{
@@ -22,8 +32,6 @@ pub fn start_thread_grep(from_aggregate: Receiver<Simplelineaggregate>, data: &P
                     writeln!(&mut stdout, "{}", &td).unwrap();               
                     stdout.set_color(ColorSpec::new().set_bg(Some(Color::Black)).set_fg(Some(Color::White)),).unwrap();                                
                     }
-                //println!("{}({})==>{}",&l.file,l.position,&l.where_to_search);
-                println!();
             }
         }
     })
--- src/read.rs
+++ src/read.rs
@@ -5,6 +5,7 @@ use std::io::BufReader;
 use std::sync::mpsc::Sender;
 use std::thread::{spawn, JoinHandle};
 use std::convert::TryInto;
+use glob::glob;
 use super::paramcli::*;
 use super::lineread::*;
 
@@ -21,35 +22,56 @@ pub fn start_thread_read(to_aggregate: Sender<Lineread>, data: &Paramcli) -> Joi
 }
 
 fn start_thread_read_files(to_aggregate: Sender<Lineread>, data: &Paramcli) -> JoinHandle<()> {
-    //TODO:input is a path either get all files or  recurse in it
-    println!("multi-file and recurse grep not yep implemented !!");
-    //TODO
-    let file = String::from(&data.input);
+    let files = String::from(&data.input);
     spawn(move || {
-        let input = File::open(&file);
-        match input {
-            Err(e) => {
-                println!("Error reading file {} => {}", &file, e);
-            }
-            Ok(f) => {
-                let buffered = BufReader::new(f);
-                for (pos, line) in buffered.lines().enumerate(){
-                    if let Ok(l) = line {
-                        let p:u32=pos.try_into().unwrap();
-                        let lr = Lineread::new(&file,p,&l);
-                        if to_aggregate.send(lr).is_err() {
-                            println!("error sending to compute");
-                            return;
+        for entry in glob(&files).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path) => {
+                    if path.is_dir() {
+                        continue
+                    }
+                    let input = File::open(&path);
+                    let file = path.to_str().unwrap();
+                    match input {
+                        Err(e) => {
+                            println!("Error reading file {} => {}", &file, e);
+                        }
+                        Ok(f) => {
+                            if !read(f, to_aggregate.clone(), file.to_string()){ return;}
+                            /*let buffered = BufReader::new(f);
+                            for (pos, line) in buffered.lines().enumerate(){
+                                if let Ok(l) = line {
+                                    let p:u32=pos.try_into().unwrap();
+                                    let lr = Lineread::new(&file,p,&l);
+                                    if to_aggregate.send(lr).is_err() {
+                                        println!("error sending to compute");
+                                        return;
+                                    }
+                                }
+                            }*/
                         }
                     }
-                }
+                },
+                Err(e) => println!("{:?}", e),
             }
         }
     })
 }
 
-
-
+fn read(f:File, to_aggregate: Sender<Lineread>,file:String)-> bool{
+    let buffered = BufReader::new(f);
+    for (pos, line) in buffered.lines().enumerate(){
+        if let Ok(l) = line {
+            let p:u32=pos.try_into().unwrap();
+            let lr = Lineread::new(&file,p,&l);
+            if to_aggregate.send(lr).is_err() {
+                println!("error sending to compute");
+                return false;
+            }
+        }
+    }
+    true
+}
 
 fn start_thread_read_file(to_aggregate: Sender<Lineread>, fic: &str) -> JoinHandle<()> {
     let file = String::from(fic);
@@ -60,7 +82,8 @@ fn start_thread_read_file(to_aggregate: Sender<Lineread>, fic: &str) -> JoinHand
                 println!("Error reading file {} => {}", &file, e);
             }
             Ok(f) => {
-                let buffered = BufReader::new(f);
+                if !read(f, to_aggregate,file){ return;}
+/*                let buffered = BufReader::new(f);
                 for (pos, line) in buffered.lines().enumerate(){
                     if let Ok(l) = line {
                         let p:u32=pos.try_into().unwrap();
@@ -70,7 +93,7 @@ fn start_thread_read_file(to_aggregate: Sender<Lineread>, fic: &str) -> JoinHand
                             return;
                         }
                     }
-                }
+                }*/
             }
         }
     })
